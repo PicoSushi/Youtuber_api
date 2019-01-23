6ec306464129e03b79d04d0b967e9b29fb1fc6e2
Jun Saitou <saitouatusi9415@gmail.com>
2019-01-23 15:53:50 +0900

    improve

--- Youtuber_api/api/models.py
+++ Youtuber_api/api/models.py
@@ -7,6 +7,9 @@ class Vtuber(models.Model):
     name1 = models.CharField(max_length=20)
     name2 = models.CharField(max_length=20)
     belong = models.CharField(max_length=20)
-    channel_id = models.CharField(max_length=20)
+    channel_id = models.CharField(max_length=40)
     youtube_url = models.CharField(max_length=80)
     tag1 = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name1
