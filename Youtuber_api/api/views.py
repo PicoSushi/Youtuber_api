34f3f776fe475c949393f7a7ef871a703aa21d7d
Jun Saitou <saitouatusi9415@gmail.com>
2019-02-01 10:46:03 +0900

    improve

--- Youtuber_api/api/views.py
+++ Youtuber_api/api/views.py
@@ -9,10 +9,10 @@ class VtuberViewSet(viewsets.ModelViewSet):
     queryset = Vtuber.objects.all()
     serializer_class = VtuberSerializer
     # api/vtuber?tag=でtag検索対応
-    filter_fields = ('name', 'tag')
+    filter_fields = ('name', 'tag', 'belong')
 
 
 class YoutuberViewSet(viewsets.ModelViewSet):
     queryset = Youtuber.objects.all()
     serializer_class = YoutuberSerializer
-    filter_fields = ('name', 'tag')
+    filter_fields = ('name', 'tag', 'belong')
